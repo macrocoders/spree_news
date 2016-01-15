2cabfb7893f14aefd2300949d9b9ce8d9c218bdd
Ivan Fefilov <ivfefilov@gmail.com>
2016-01-15 12:28:59 +0400

    show ticker tab only for admin user

--- app/overrides/spree_news_overrides.rb
+++ app/overrides/spree_news_overrides.rb
@@ -1,5 +1,4 @@
 Deface::Override.new(virtual_path: 'spree/admin/shared/_menu',
                      name: 'news_admin_tab',
                      insert_bottom: "[data-hook='admin_tabs']",
-                     text: "<%= tab(:news_index) %>",
-                     disabled: false)
+                     partial: 'shared/ticker_news_tab')
